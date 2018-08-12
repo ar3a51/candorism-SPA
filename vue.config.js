08adf4705010611ad6ceda8c9dfd842413c450c6
ar3a51 <ar3a51@yahoo.com>
2018-08-12 18:54:29 +1000

    modify the copy webpack plugin

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,23 @@
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
 module.exports = {
     configureWebpack: {
         entry: [
             "./src/app/main.js"
+        ],
+        plugins:[
+        new CopyWebpackPlugin(
+            [
+              {
+                from: './public',
+                to: './dist',
+                ignore: [
+                  'index.html',
+                  '.DS_Store'
+                ]
+              }
+            ]
+          )
         ]
     }
 }
