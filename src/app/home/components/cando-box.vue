f4c84592d591d00f1aa5ffa628b0b6dc12bf790a
ar3a51 <ar3a51@yahoo.com>
2018-08-19 15:00:53 +1000

    Adding additional elements

---
+++ src/app/home/components/cando-box.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="box">
+
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="scss" scoped>
+    .box {
+        border-width: .1em;
+        border-style: solid;
+        border-color: #ccc;
+
+        height: 300px;
+        width: 400px;
+    }
+</style>
--- src/app/home/home2.vue
+++ src/app/home/home2.vue
@@ -1,10 +1,26 @@
 <template>
     <main>
         <header>
-            <div class="header col-xs-12"></div>
+            <div class="header col-12">
+                <logo />
+                <nav-panel />
+            </div>
         </header>
         <article>
-            <div class="banner col-xs-12"></div>
+            <div class="banner col-12">
+
+            </div>
+            <div class="body col-12">
+                <div class="col-12 col-sm-12 col-md-12 col-lg-4 col-xl-4 float-left">
+                    <cando-box />
+                </div>
+                  <div class="col-12 col-sm-12 col-md-12 col-lg-4 col-xl-4 float-left">
+                    <cando-box />
+                </div>
+                  <div class="col-12 col-sm-12 col-md-12 col-lg-4 col-xl-4 float-left">
+                    <cando-box />
+                </div>
+            </div>
             <div class="login-box 
                         d-none 
                         d-sm-none 
@@ -22,14 +38,31 @@
     </main>
 </template>
 <script>
+
 const loginForm = () => import("./components/login-form");
+const logo = () => import("./components/logo");
+const navPanel = () => import ("./components/nav-panel");
+const candoBox = () => import ("./components/cando-box");
+
 export default {
     components:{
-        loginForm
+        loginForm,
+        logo,
+        navPanel,
+        candoBox,
     }
 }
 </script>
 <style lang="scss" scoped>
+
+.float-left {
+    float: left;
+}
+
+.float-right {
+    float: right;
+}
+
 header {
     height: auto;
     float: left;
@@ -41,6 +74,8 @@ header {
         background: #ffffff;
         box-shadow: 2px 1px 5px 6px #ccc;
         padding: 0px;
+        display: flex;
+
     }
 }
 article {
@@ -52,11 +87,21 @@ article {
         height: 400px;
     }
 
+    .body {
+        padding-top: 40px;
+        height: 400px;
+
+        div {
+            margin-left: auto;
+            margin-right: auto;
+        }
+    }
+
     .login-box{
         position: absolute;
         z-index: 1000;
         border-radius: 4px;
-        top: 30%;
+        top: 15%;
         right: 10%;
         width: 400px;
         height: auto;
@@ -70,7 +115,7 @@ article {
         right: auto;
         z-index: 1000;
         width: 70%;
-        top: 30%;
+        top: 15%;
         background-color: white;
         border-radius: 4px;
         box-shadow: 2px 0px 5px 2px #ccc;
